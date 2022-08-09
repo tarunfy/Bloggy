4065fff6ca8f9597f9d62630245b5ea3f6b7d20d
tarunfy <tarunsharma8920@gmail.com>
2022-08-09 22:14:20 +0530

    created profile page

--- frontend/pages/profile/[username].jsx
+++ frontend/pages/profile/[username].jsx
@@ -1,6 +1,11 @@
 import { Avatar, Button, useColorMode } from "@chakra-ui/react";
 import Navbar from "../../components/Navbar";
 import Layout from "../../components/Layout";
+import Link from "next/link";
+import { RiCake2Line } from "react-icons/ri";
+import { FiMail } from "react-icons/fi";
+import { BsGlobe } from "react-icons/bs";
+import { FaGithub, FaTwitter } from "react-icons/fa";
 
 const Profile = () => {
   //Change layout title to current user's name.
@@ -15,15 +20,15 @@ const Profile = () => {
       <Navbar />
 
       <div
-        className={`max-w-[900px] p-10 rounded-lg relative mx-auto bg-red-50 mt-52 ${
+        className={`max-w-[900px] border p-10 rounded-lg relative mx-auto bg-red-50 mt-52 ${
           colorMode === "light"
             ? "!border-gray-300 !bg-white/50"
             : "!border-gray-50/70 !bg-[#2D3748]/50"
         }`}
       >
         <div
-          className={`rounded-full p-1 absolute -top-10 left-2/4 -translate-x-[50%] ${
-            colorMode === "light" ? "bg-[#fafbfb]" : "bg-[#283040]"
+          className={`rounded-full p-1 absolute -top-12 left-2/4 -translate-x-[50%] ${
+            colorMode === "light" ? " !bg-[#d8dadb80] " : " !bg-[#1A202C]"
           }`}
         >
           <Avatar
@@ -33,9 +38,70 @@ const Profile = () => {
           />
         </div>
         <div className="w-full flex justify-end">
-          <Button className="hover:!bg-[#4A18D7] !bg-[#5d2ee0] text-white">
-            Edit profile
-          </Button>
+          <Link href="/settings">
+            <a>
+              <Button className="hover:!bg-[#4A18D7] !bg-[#5d2ee0] text-white">
+                Edit profile
+              </Button>
+            </a>
+          </Link>
+        </div>
+        <div className="text-center mt-5 space-y-6 px-10">
+          <div>
+            <h1 className="font-bold text-3xl">Tarun Sharma</h1>
+            <p className="text-lg">
+              18 | An Enthusiastic full-stack developer and a learner. Focused
+              on ⚛️. Build apps with 💖. Let's connect 🍻
+            </p>
+          </div>
+          <div className="flex items-center justify-around space-x-3">
+            <div className="flex items-center space-x-2">
+              <RiCake2Line />
+              <p>Joined on 1 Jan 2020</p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <FiMail />
+              <a href="mailto:tarunsharma8920@gmail.com">
+                tarunsharma8920@gmail.com
+              </a>
+            </div>
+            <div className="flex items-center space-x-2">
+              <BsGlobe />
+              <a href="https://tarunsharma.vercel.app/" target="_blank">
+                tarunsharma.vercel.app/
+              </a>
+            </div>
+            <div className="flex items-center space-x-2 text-xl">
+              <a href="https://github.com/tarunfy" target="_blank">
+                <FaGithub />
+              </a>
+              <a href="https://twitter.com/tarunfy" target="_blank">
+                <FaTwitter />
+              </a>
+            </div>
+          </div>
+          <div className="flex items-center justify-evenly">
+            <div>
+              <h4
+                className={`text-sm ${
+                  colorMode === "dark" ? "text-white/50" : "text-black/50"
+                }`}
+              >
+                Education
+              </h4>
+              <p>Aster public school</p>
+            </div>
+            <div>
+              <h4
+                className={`text-sm ${
+                  colorMode === "dark" ? "text-white/50" : "text-black/50"
+                }`}
+              >
+                Work
+              </h4>
+              <p>Student</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
