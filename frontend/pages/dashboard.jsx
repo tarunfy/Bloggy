3c8de9d6778023c2de04cc72609154f7d78a59fd
tarunfy <tarunsharma8920@gmail.com>
2022-08-09 23:14:58 +0530

    changes

--- frontend/pages/dashboard.jsx
+++ frontend/pages/dashboard.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import BlogCard from "../components/Card/BlogCard";
+import Layout from "../components/Layout";
 
 const Dashboard = () => {
   const [blogs, setBlogs] = useState([1, 2, 3]);
   return (
     <div>
+      <Layout
+        title="Dashboard - Bloggy Community"
+        description="Dashboard of bloggy community"
+      />
       <Navbar />
       <div className="mt-44 max-w-[1000px] mx-auto">
         <h1 className="text-3xl font-semibold mb-5">
